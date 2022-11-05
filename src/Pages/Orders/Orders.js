7ad4cf7bb756bd0d0b6fe18bf47bad92bb223630
arifuddinraihan <mdarifuddinraihan@gmail.com>
2022-11-05 17:47:04 +0600

    added patch api routing, patch body status and added event handle for update

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -26,12 +26,35 @@ const Orders = () => {
                 .then(data => {
                     console.log(data);
                     if (data.deletedCount > 0){
-                        alert("Delete Successfully")
                         const remaining = orders.filter(odr => odr._id !== id)
                         setOrders(remaining)
+                        // alert("Delete Successfully")
                     }
                 })
         }
+    }
+
+    const handleStatusUpdate = id => {
+        console.log("status button working", id)
+        fetch(`http://localhost:5000/orders/${id}`, {
+            method: "PATCH",
+            headers : {
+                "content-type" : "application/json"
+            },
+            body : JSON.stringify({status : "Approved"})
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.modifiedCount > 0) {
+                    const remaining = orders.filter(odr => odr._id !== id)
+                    const approving = orders.find(odr => odr._id === id)
+                    approving.status = 'Approved';
+                    const newOrders = [approving,...remaining]
+                    setOrders(newOrders)
+                    // alert("Updated Successfully")
+                }
+            })
     }
 
     return (
@@ -59,6 +82,7 @@ const Orders = () => {
                                 key={order._id}
                                 order={order}
                                 handleDelete={handleDelete}
+                                handleStatusUpdate={handleStatusUpdate}
                                 ></OrderRow>)
                         }
                     </tbody>
