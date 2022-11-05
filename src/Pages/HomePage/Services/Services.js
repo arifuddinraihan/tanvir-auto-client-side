0a8ccd7cfc07d432e8f26d39e607656e11122d1d
arifuddinraihan <mdarifuddinraihan@gmail.com>
2022-11-05 21:48:01 +0600

    successfully added pagination format along with size of service per page

--- src/Pages/HomePage/Services/Services.js
+++ src/Pages/HomePage/Services/Services.js
@@ -1,31 +1,63 @@
 import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import ServiceCard from './ServiceCard';
 
 const Services = () => {
-    const [services, setServices] = useState([]);
+    const [allServices, setAllServices] = useState([]);
+    const [count, setCount] = useState(0);
+    const [page, setPage] = useState(0);
+    const [size, setSize] = useState(2);
+
+    const pages = Math.ceil(count / size)
+
     useEffect(() => {
         fetch('http://localhost:5000/services')
             .then(res => res.json())
             .then(data => {
-                setServices(data)
+                const { count, services } = data
+                setCount(count)
+                setAllServices(services)
             })
     }, [])
 
     return (
-        <div>
+        <div className='flex flex-col items-center'>
             <div className='text-center my-8'>
                 <p className="text-2xl font-bold text-orange-600">Services</p>
                 <h2 className="text-5xl font-semibold">Our Service Area</h2>
                 <p>the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
             </div>
             <div className='text-center py-6 mx-8'>
-                <h2 className='font-semibold text-orange-600 py-5'>Total Services: {services.length}</h2>
+                <h2 className='font-semibold text-orange-600 py-5'>Total Services: {allServices.length}</h2>
                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
                     {
-                        services.map(service => <ServiceCard 
+                        allServices.map(service => <ServiceCard
                             key={service._id}
                             service={service}></ServiceCard>)
                     }
+                </div>
+            </div>
+            <div className='flex gap-8 my-4 mx-auto'>
+                <div>
+                    <p>Selected Page : {page} & Size : {size}</p>
+                    <div className="tabs bg-amber-300">
+                        {
+                            [...Array(pages).keys()].map(number =>
+                                <NavLink
+                                    onClick={() => setPage(number)}
+                                    className={page === number ? 'tab tab-lifted tab-active' : 'tab tab-lifted'}>
+                                    {number}
+                                </NavLink>)
+                        }
+                    </div>
+                </div>
+                <div>
+                    <select onChange={event => setSize(event.target.value)}
+                    className="select select-warning w-full max-w-xs">
+                        <option value="2" >2</option>
+                        <option value="3" selected>3</option>
+                        <option value="6" >6</option>
+                    </select>
                 </div>
             </div>
         </div>
